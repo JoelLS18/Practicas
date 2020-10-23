aea6ea4e72930d9543f2d2f19ad4ff249c23eddf
JoelLS18 <71998374+JoelLS18@users.noreply.github.com>
2020-10-22 23:21:07 -0500

    Add files via upload

--- USART_EN_C.X/usart.c
+++ USART_EN_C.X/usart.c
@@ -1,27 +1,22 @@
-/*
- * File:   primerintento.c
- * Author: ferma
- *
- * Created on 29 de septiembre de 2020, 12:10 AM
- */
+
 #include <xc.h>
 #include<stdio.h>
 #define _XTAL_FREQ 8000000
 // CONFIG1
-#pragma config FOSC = INTRC_CLKOUT// Oscillator Selection bits (INTOSC oscillator: CLKOUT function on RA6/OSC2/CLKOUT pin, I/O function on RA7/OSC1/CLKIN)
-#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled and can be enabled by SWDTEN bit of the WDTCON register)
-#pragma config PWRTE = ON       // Power-up Timer Enable bit (PWRT enabled)
-#pragma config MCLRE = ON       // RE3/MCLR pin function select bit (RE3/MCLR pin function is MCLR)
-#pragma config CP = OFF         // Code Protection bit (Program memory code protection is disabled)
-#pragma config CPD = OFF        // Data Code Protection bit (Data memory code protection is disabled)
-#pragma config BOREN = ON       // Brown Out Reset Selection bits (BOR enabled)
-#pragma config IESO = ON        // Internal External Switchover bit (Internal/External Switchover mode is enabled)
-#pragma config FCMEN = ON       // Fail-Safe Clock Monitor Enabled bit (Fail-Safe Clock Monitor is enabled)
-#pragma config LVP = OFF        // Low Voltage Programming Enable bit (RB3 pin has digital I/O, HV on MCLR must be used for programming)
+#pragma config FOSC = INTRC_CLKOUT
+#pragma config WDTE = OFF       
+#pragma config PWRTE = ON       
+#pragma config MCLRE = ON       
+#pragma config CP = OFF         
+#pragma config CPD = OFF       
+#pragma config BOREN = ON      
+#pragma config IESO = ON        
+#pragma config FCMEN = ON       
+#pragma config LVP = OFF       
 
 // CONFIG2
-#pragma config BOR4V = BOR40V   // Brown-out Reset Selection bit (Brown-out Reset set to 4.0V)
-#pragma config WRT = OFF        // Flash Program Memory Self Write Enable bits (Write protection off)
+#pragma config BOR4V = BOR40V   
+#pragma config WRT = OFF        
 unsigned char entrada;
 void __interrupt() usart(void){
       if(PIR1bits.RCIF==1){
